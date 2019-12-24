6c699503c1beace6adae282365dcc2a5385704bb
Fran <franciscopossetto@hotmail.com>
2019-12-24 09:50:25 -0300

    Upload Properties. Post Build Commands have added.

--- RevitAPIExtension/RevitAPIExtensionPackage.cs
+++ RevitAPIExtension/RevitAPIExtensionPackage.cs
@@ -25,6 +25,7 @@ namespace RevitAPIExtension
     /// </remarks>
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [Guid(RevitAPIExtensionPackage.PackageGuidString)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
     public sealed class RevitAPIExtensionPackage : AsyncPackage
     {
         /// <summary>
@@ -45,7 +46,10 @@ namespace RevitAPIExtension
         {
             // When initialized asynchronously, the current thread may be a background thread at this point.
             // Do any initialization that requires the UI thread after switching to the UI thread.
+            
+
             await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            await Command.InitializeAsync(this);
         }
 
         #endregion
---
+++ RevitAPIExtension/Terminal/Terminal.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPIExtension
+{
+    public static class Terminal
+    {
+        public static string Location = @"C:\PyRevit\PyRevitScaffold";
+        public static bool Install() 
+        {
+            try
+            {
+                string path = Environment.GetEnvironmentVariable("Path", EnvironmentVariableTarget.User);
+                string new_path = path + ";" + Location;
+                Environment.SetEnvironmentVariable("Path", new_path, EnvironmentVariableTarget.User);
+                return true;
+            } catch(Exception e)
+            {
+                return false;
+            }
+            
+        }
+
+        public static bool IsInstall()
+        {
+            string path = Environment.GetEnvironmentVariable("Path", EnvironmentVariableTarget.User);
+            List<string> paths = path.Split(';').ToList();
+            if (paths.Any(x => x == Location))
+                return true;
+            else
+                return false;
+        }
+    }
+}

ebf4bb6eeb0273e44853cd7b6d44a8739538199f
Francisco Possetto <franciscopossetto@hotmail.com>
2020-11-01 18:23:27 -0300

    -add xaml code in new UI

--- RevitAPIExtension/Interfaces/AddRibbonItem.xaml.cs
+++ RevitAPIExtension/Interfaces/AddRibbonItem.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RevitAPIExtension.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,64 @@ namespace RevitAPIExtension.Interfaces
     /// </summary>
     public partial class AddRibbonItem : Window
     {
-        public AddRibbonItem()
+        private bool IsCancel = false;
+        private readonly UIDefaultData _defaultData;
+        private readonly CodeGenData _data;
+        public AddRibbonItem(UIDefaultData defaultData)
         {
             InitializeComponent();
+            _data = defaultData.Default;
+            _defaultData = defaultData;
+            cbPanels.ItemsSource = defaultData.Panels.Select(p => p.Name);
+            cbPanels.SelectedIndex = 0;
+            cbType.SelectedIndex = 0;
+            tbText.Text = _data.Text;
+        }
+
+        public new CodeGenData ShowModal()
+        {
+            base.ShowModal();
+            return IsCancel ? null : _data;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _data.Text = tbText.Text;
+            _data.Panel = cbPanels.SelectedItem as String;
+            var pullSelected = cbPullDows.SelectedItem;
+            _data.Parent = pullSelected as ButtonReference;
+            _data.Type = (ButtonType)(cbType.SelectedIndex as int? ?? 0);
+            base.Hide();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            IsCancel = true;
+            base.Hide();
+        }
+
+        private void cbPanels_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            cbPullDows.IsEnabled = true;
+            string panelName = cbPanels.SelectedItem as String;
+            var panel = _defaultData.Panels.Find(p => p.Name == panelName);
+            var pullDowns = panel.PullDowns.Select(p =>
+            {
+                var uniqueName = p.Name.Value;
+                var name = uniqueName.Split('.')[0];
+                return new ButtonReference()
+                {
+                    Name = name,
+                    UniqueName = uniqueName
+                };
+            });
+            if (pullDowns.Count() > 0)
+                cbPullDows.ItemsSource = pullDowns;
+            else
+            {
+                cbPullDows.ItemsSource = null;
+                cbPullDows.IsEnabled = false;
+            }
         }
 
     }

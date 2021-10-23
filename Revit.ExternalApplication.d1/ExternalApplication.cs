d9a2f27e08dca6acd1411071325c72ccb394287e
Juan Costamagna <juancostamagna1986@gmail.com>
2021-10-22 23:46:35 -0300

    4th item added

---
+++ Revit.ExternalApplication.d1/ExternalApplication.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB.Events;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.Attributes;
+
+namespace $safeitemname$
+{
+    [Transaction(TransactionMode.Manual)]
+    public class $safeitemname$ : IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+
+            return Result.Succeeded;
+        }
+    }
+}
+
+
